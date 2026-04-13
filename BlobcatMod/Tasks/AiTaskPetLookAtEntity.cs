1319333f7e40c3ff908ca3ac82fd20d4d5a7e72e
Mononospace <38171201@users.noreply.github.com>
2026-04-13 17:24:49 +0900

    Update for 1.22

--- BlobcatMod/Tasks/AiTaskPetLookAtEntity.cs
+++ BlobcatMod/Tasks/AiTaskPetLookAtEntity.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
@@ -10,74 +6,52 @@ using Vintagestory.GameContent;
 
 namespace BlobcatMod.Tasks {
     public class AiTaskPetLookAtEntity : AiTaskBaseTargetable {
-        public bool manualExecute;
-        public float moveSpeed = 0.02f;
         public float seekingRange = 25f;
-        public float maxFollowTime = 60;
 
-        float minTurnAnglePerSec;
-        float maxTurnAnglePerSec;
-        float curTurnRadPerSec;
+        protected float minTurnAnglePerSec;
+        protected float maxTurnAnglePerSec;
 
-        float maxTurnAngleRad;
-        float spawnAngleRad;
+        protected float turnRadPerSec;
 
         public AiTaskPetLookAtEntity(EntityAgent entity, JsonObject taskConfig, JsonObject aiConfig) : base(entity, taskConfig, aiConfig) {
-            maxTurnAngleRad = taskConfig["maxTurnAngleDeg"].AsFloat(360) * GameMath.DEG2RAD;
-            spawnAngleRad = entity.Attributes.GetFloat("spawnAngleRad");
             seekingRange = taskConfig["seekingRange"].AsFloat(25.0f);
         }
 
         public override bool ShouldExecute() {
-            if (!manualExecute) {
-                targetEntity = partitionUtil.GetNearestEntity(entity.ServerPos.XYZ, seekingRange, (e) => IsTargetableEntity(e, seekingRange), EnumEntitySearchType.Creatures);
-                return targetEntity != null;
-            }
-            return false;
-        }
-
-        public float MinDistanceToTarget() {
-            return System.Math.Max(0.8f, targetEntity.SelectionBox.XSize / 2 + entity.SelectionBox.XSize / 2);
+            if (!PreconditionsSatisfied()) return false;
+            targetEntity = partitionUtil.GetNearestEntity(entity.Pos.XYZ, seekingRange, (e) => IsTargetableEntity(e, seekingRange), EnumEntitySearchType.Creatures);
+            return targetEntity != null;
         }
 
         public override void StartExecute() {
             base.StartExecute();
 
-            if (entity?.Properties.Server?.Attributes != null) {
-                minTurnAnglePerSec = (float)entity.Properties.Server.Attributes.GetTreeAttribute("pathfinder").GetFloat("minTurnAnglePerSec", 250);
-                maxTurnAnglePerSec = (float)entity.Properties.Server.Attributes.GetTreeAttribute("pathfinder").GetFloat("maxTurnAnglePerSec", 450);
-            }
-            else {
-                minTurnAnglePerSec = 250;
-                maxTurnAnglePerSec = 450;
-            }
+            var pathfinder = entity?.Properties.Server?.Attributes?.GetTreeAttribute("pathfinder");
 
-            curTurnRadPerSec = minTurnAnglePerSec + (float)entity.World.Rand.NextDouble() * (maxTurnAnglePerSec - minTurnAnglePerSec);
-            curTurnRadPerSec *= GameMath.DEG2RAD * 50 * 0.02f;
+            minTurnAnglePerSec = pathfinder?.GetFloat("minTurnAnglePerSec", 250) ?? 250;
+            maxTurnAnglePerSec = pathfinder?.GetFloat("maxTurnAnglePerSec", 450) ?? 450;
+
+            var turnAnglePerSec = minTurnAnglePerSec + entity.World.Rand.NextDouble() * (maxTurnAnglePerSec - minTurnAnglePerSec);
+            turnRadPerSec = (float)turnAnglePerSec * GameMath.DEG2RAD;
         }
 
         public override bool ContinueExecute(float dt) {
-            if (!IsInValidDayTimeHours(false)) return false;
-
-            if (entity.ServerPos.SquareDistanceTo(targetEntity.ServerPos) > seekingRange * seekingRange) {
+            if (entity.Pos.SquareDistanceTo(targetEntity.Pos) > seekingRange * seekingRange) {
                 return false;
             }
-            Vec3f targetVec = new Vec3f();
 
-            targetVec.Set(
-                (float)(targetEntity.ServerPos.X - entity.ServerPos.X),
-                (float)(targetEntity.ServerPos.Y - entity.ServerPos.Y),
-                (float)(targetEntity.ServerPos.Z - entity.ServerPos.Z)
+            Vec3f targetVec = new Vec3f(
+                (float)(targetEntity.Pos.X - entity.Pos.X),
+                (float)(targetEntity.Pos.Y - entity.Pos.Y),
+                (float)(targetEntity.Pos.Z - entity.Pos.Z)
             );
 
-            float desiredYaw = (float)Math.Atan2(targetVec.X, targetVec.Z);
+            float targetYaw = (float)Math.Atan2(targetVec.X, targetVec.Z);
 
-            desiredYaw = GameMath.Clamp(desiredYaw, spawnAngleRad - maxTurnAngleRad, spawnAngleRad + maxTurnAngleRad);
-
-            float yawDist = GameMath.AngleRadDistance(entity.ServerPos.Yaw, desiredYaw);
-            if (Math.Abs(yawDist) > 0.01) {
-                entity.ServerPos.Yaw += GameMath.Clamp(yawDist, -curTurnRadPerSec * dt, curTurnRadPerSec * dt);
-                entity.ServerPos.Yaw = entity.ServerPos.Yaw % GameMath.TWOPI;
+            float yawDistance = GameMath.AngleRadDistance(entity.Pos.Yaw, targetYaw);
+            if (Math.Abs(yawDistance) > 0.01) {
+                entity.Pos.Yaw += GameMath.Clamp(yawDistance, -turnRadPerSec * dt, turnRadPerSec * dt);
+                entity.Pos.Yaw = entity.Pos.Yaw % GameMath.TWOPI;
             }
 
             return true;
